8a6dd632b250bb435cb8523923e97323063a1c54
Derek Powell <derekpowll@gmail.com>
2024-02-27 11:32:00 -0700

    adding easyeditor

---
+++ easyeditor/custom/loaders.py
@@ -0,0 +1,49 @@
+import pandas as pd
+
+from ast import literal_eval
+
+def load_data(data_path = "../catco-data/"):
+
+    data_dict = dict()
+
+    # types_df = pd.read_csv(data_path + "animal-type-tokens.tsv", sep="\t")
+    # properties_df = pd.read_csv(data_path + "animal-data.tsv", sep="\t")
+
+    # edits_df = pd.read_csv(data_path + "edits.csv")
+    # baseline_df = pd.read_csv(data_path + "baseline-evaluation.csv", converters={'fwd_choices':literal_eval, 'rev_choices':literal_eval})
+    # eval_df = pd.read_csv( data_path + "edits-evaluation.csv", converters={'fwd_choices':literal_eval, 'rev_choices':literal_eval})
+
+    edits_df = pd.read_json(data_path + "edits.json")
+    baseline_df = pd.read_json(data_path + "baseline-evaluation.json")
+    eval_df = pd.read_json(data_path + "edits-evaluation.json")
+
+    return baseline_df, edits_df, eval_df
+
+
+import configparser
+
+def auth_token():
+
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    return config["hugging_face"]["token"]
+
+
+def load_prefixes(prefix_lines = -1, verbose=False):
+    with open('prefix_fwd.txt') as f:
+        prefix_fwd = "".join(f.readlines()[0:prefix_lines])
+
+    with open('prefix_rev.txt') as f:
+        prefix_rev = "".join(f.readlines()[0:prefix_lines])
+
+    with open('single-prefix2.txt') as f:
+        prefix_single = "".join(f.readlines()[0:prefix_lines])
+
+    # prefix_fwd = f.read()
+    if verbose:
+        print(prefix_fwd)
+        print("---")
+        print(prefix_rev)
+        print("---")
+
+    return prefix_fwd, prefix_rev, prefix_single
